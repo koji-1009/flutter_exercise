d3b31b077bd2e4a5912ec48d4666b724431f8c6c
Koji Wakamiya <koji.wakamiya@gmail.com>
2022-08-07 19:27:39 +0900

    Add http requests

---
+++ cat_pics_riverpod/lib/logic/cataas_service.dart
@@ -0,0 +1,41 @@
+import 'dart:convert';
+
+import 'package:cat_pics_riverpod/model/response.dart';
+import 'package:http/http.dart' as http;
+
+class CataasService {
+  CataasService({
+    required this.client,
+  });
+
+  final http.Client client;
+
+  Future<TagList> tags() async {
+    final uri = Uri.https(
+      'cataas.com',
+      'api/tags',
+    );
+    final response = await client.get(uri);
+
+    return TagList.fromJson({
+      'tags': jsonDecode(response.body),
+    });
+  }
+
+  Future<CatList> cats({
+    required List<String> tags,
+  }) async {
+    final uri = Uri.https(
+      'cataas.com',
+      'api/cats',
+      {
+        'tags': tags.join(','),
+      },
+    );
+    final response = await client.get(uri);
+
+    return CatList.fromJson({
+      'cats': jsonDecode(response.body),
+    });
+  }
+}
