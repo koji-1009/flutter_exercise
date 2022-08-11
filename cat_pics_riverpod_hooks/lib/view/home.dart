522edfd81e652c3dc9c106012b59c1c140508800
Koji Wakamiya <koji.wakamiya@gmail.com>
2022-08-11 21:04:29 +0900

    Use hooks

--- cat_pics_riverpod_hooks/lib/view/home.dart
+++ cat_pics_riverpod_hooks/lib/view/home.dart
@@ -1,19 +1,19 @@
-import 'package:cat_pics_riverpod_hooks/logic/home_state.dart';
 import 'package:cat_pics_riverpod_hooks/view/home_future_builder.dart';
 import 'package:cat_pics_riverpod_hooks/view/home_future_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class HomePage extends ConsumerWidget {
+class HomePage extends HookConsumerWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final tabIndex = ref.watch(homeTabProvider);
+    final tabIndex = useState(0);
 
-    final title = tabIndex == 0 ? 'FutureBuilder' : 'FutureProvider';
-    final body = tabIndex == 0
+    final title = tabIndex.value == 0 ? 'FutureBuilder' : 'FutureProvider';
+    final body = tabIndex.value == 0
         ? const HomeFutureBuilderWidget()
         : const HomeFutureProviderWidget();
 
@@ -31,9 +31,9 @@ class HomePage extends ConsumerWidget {
       ),
       body: body,
       bottomNavigationBar: BottomNavigationBar(
-        currentIndex: tabIndex,
+        currentIndex: tabIndex.value,
         onTap: (index) {
-          ref.read(homeTabProvider.notifier).state = index;
+          tabIndex.value = index;
         },
         items: const [
           BottomNavigationBarItem(
